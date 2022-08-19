c790674dfba14582104a960874e0f6440221bdc9
nick <npitoniak00@gmail.com>
2022-08-18 23:33:17 -0400

    correct inverted z axis

--- Bound.cpp
+++ Bound.cpp
@@ -24,7 +24,7 @@ vector<vector<int>> Bound::get_dim_bounds() {
 bool Bound::is_vec3_within_bounds(glm::vec3 input_vec3) {
   if(bounds.size() < 1) { return false; }
   if(input_vec3[0] <= bounds[0][1] && input_vec3[0] >= bounds[0][0]) {
-    if(1==1 || (input_vec3[1] <= bounds[1][1] && input_vec3[1] >= bounds[1][0])) {
+    if(1 == 1 || (input_vec3[1] <= bounds[1][1] && input_vec3[1] >= bounds[1][0])) {
       if(input_vec3[2] <= bounds[2][1] && input_vec3[2] >= bounds[2][0]) {
         return true;
       }
@@ -41,21 +41,27 @@ void Bound::generate_rectangular_bounds(glm::vec3 max_positive_corner,glm::vec3 
   rectangular_bounds.clear();
   if(is_horizontal) { // horizontal rectangular bounds (ex: floor)
     rectangular_bounds.push_back(max_positive_corner);
-    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1],(float)(max_positive_corner[2]-dim_deltas[2])));
+    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0]-dim_deltas[0],(float)max_positive_corner[1],(float)max_positive_corner[2]));
+    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0]-dim_deltas[0],(float)max_positive_corner[1],(float)max_positive_corner[2]-dim_deltas[2]));
+    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1],(float)max_positive_corner[2]-dim_deltas[2]));
+    /*rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1],(float)(max_positive_corner[2]-dim_deltas[2])));
     rectangular_bounds.push_back(glm::vec3((float)(max_positive_corner[0]-dim_deltas[0]),(float)max_positive_corner[1],(float)(max_positive_corner[2]-dim_deltas[2])));
-    rectangular_bounds.push_back(glm::vec3((float)(max_positive_corner[0]-dim_deltas[0]),(float)max_positive_corner[1],(float)max_positive_corner[2]));
+    rectangular_bounds.push_back(glm::vec3((float)(max_positive_corner[0]-dim_deltas[0]),(float)max_positive_corner[1],(float)max_positive_corner[2]));*/
   } else { // vertical rectangular bounds (ex: wall)
     rectangular_bounds.push_back(max_positive_corner);
+    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1],(float)max_positive_corner[2]-dim_deltas[2]));
+    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1]-dim_deltas[1],(float)max_positive_corner[2]-dim_deltas[2]));
     rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1]-dim_deltas[1],(float)max_positive_corner[2]));
+    /*rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1]-dim_deltas[1],(float)max_positive_corner[2]));
     rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1]-dim_deltas[1],(float)(max_positive_corner[2]-dim_deltas[2])));
-    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1],(float)(max_positive_corner[2]-dim_deltas[2])));
+    rectangular_bounds.push_back(glm::vec3((float)max_positive_corner[0],(float)max_positive_corner[1],(float)(max_positive_corner[2]-dim_deltas[2])));*/
   }
   generate_random_inclusive_position();
 }
 glm::vec3 Bound::generate_random_inclusive_position() {
   if(is_horizontal) {
     vector<int> x_limits;
-    x_limits.push_back(rectangular_bounds[3][0]);
+    x_limits.push_back(rectangular_bounds[1][0]);
     x_limits.push_back(rectangular_bounds[0][0]);
     bounds.push_back(x_limits);
     vector<int> y_limits;
@@ -63,7 +69,7 @@ glm::vec3 Bound::generate_random_inclusive_position() {
     y_limits.push_back(rectangular_bounds[0][1]);
     bounds.push_back(y_limits);
     vector<int> z_limits;
-    z_limits.push_back(rectangular_bounds[1][2]);
+    z_limits.push_back(rectangular_bounds[3][2]);
     z_limits.push_back(rectangular_bounds[0][2]);
     bounds.push_back(z_limits);
   } else {
@@ -73,7 +79,7 @@ glm::vec3 Bound::generate_random_inclusive_position() {
     bounds.push_back(x_limits);
     vector<int> y_limits;
     y_limits.push_back(rectangular_bounds[0][1]);
-    y_limits.push_back(rectangular_bounds[0][1]);
+    y_limits.push_back(rectangular_bounds[3][1]);
     bounds.push_back(y_limits);
     vector<int> z_limits;
     z_limits.push_back(rectangular_bounds[1][2]);
